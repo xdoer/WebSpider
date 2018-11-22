b7b90cd59800c51d2629aa5c04e1ffd376bb06b9
luckyhh <gotoanything@foxmail.com>
2018-11-22 10:04:13 +0800

    add

--- conf/conf.js
+++ conf/conf.js
@@ -1,4 +1,7 @@
 module.exports = {
-    DataBaseUrl: "localhost:27017/websplider",
-    HOSTNAME: "http://localhost:3000"
+    // DataBaseUrl: "localhost:27017/websplider",
+    // HOSTNAME: "http://localhost:3000"
+
+    DataBaseUrl: "luckyhh:hj782161@ds147890.mlab.com:47890/websplider",
+    HOSTNAME: "https://splider.herokuapp.com"
 }
