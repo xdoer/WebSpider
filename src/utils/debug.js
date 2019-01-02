d9f9a250d8aa60b289f98e72529ac1d6793280cd
LuckyHH <gotoanything@foxmail.com>
2019-01-02 19:15:51 +0800

    更新:整合内容到debug.js中

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -4,10 +4,33 @@
  * LOG模式下，将程序运行状态写入文件
  */
 const { DEBUG, LOG } = require('../config')
-const _note = require('./note')
 const _time = require('./time')
+const fs = require('fs')
+const path = require('path')
 
-/** 
+/**
+ * 追加文本内容
+ * @param {string} content - 输出的内容
+ * @param {string} _path - 路径参数(error,running)
+ */
+const writeFile = (content, _path) => {
+  const t = new Date()
+  const filename = `${t.getFullYear()}${(t.getMonth() + 1).toString().padStart(2, 0)}${t.getDate().toString().padStart(2, 0)}.txt`
+  fs.appendFile(path.resolve(__dirname, `../../log/${_path}/${filename}`), `${_time()} ${content} \n`, err => {
+    if (err) console.log(err)
+  })
+}
+
+const _note = {
+  error (content) {
+    writeFile(content, 'error')
+  },
+  log (content) {
+    writeFile(content, 'running')
+  }
+}
+
+/**
  * content为要输出的内容
  * err参数为错误输出标志位
  */
