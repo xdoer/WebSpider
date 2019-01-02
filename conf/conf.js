a8bf40a5ed1bffc006884bd7ba86d68dc351f147
韩前进 <qianjin.han@hand-china.com>
2019-01-02 11:31:32 +0800

    删除:重构代码

--- conf/conf.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    DataBaseUrl: "localhost:27017/websplider",
-    HOSTNAME: "http://localhost:3000"
-}
--- conf/session_conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    key: 'Authorization',
-    maxAge: 3600000,
-    overwrite: true,
-    httpOnly: true,
-    signed: true,
-    rolling: false,
-    renew: false,
-};
