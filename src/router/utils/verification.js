3a970c05766083832453416fddf2050cae54b6c1
LuckyHH <gotoanything@foxmail.com>
2019-01-08 01:13:31 +0800

    更新:添加验证字段,优化验证方案

--- src/router/utils/verification.js
+++ src/router/utils/verification.js
@@ -4,11 +4,11 @@
  */
 const { _filter: { filterEval, filterUrl }, _debug } = require('../../utils')
 
-module.exports = ({ url, tags, depth, form, charset, proxyMode, proxies, mode, start, end }) => {
+module.exports = ({ url, tags, depth, form, charset, proxyMode, proxies, mode, start, end, interval }) => {
   const isType = Object.prototype.toString
   // 判断是否为字符串
-  const strAry = [url, depth, charset, proxyMode, mode, start, end]
-  if (strAry.some(n => isType.call(n)) !== '[object String]') {
+  const strAry = [url, depth, charset, proxyMode, mode, start, end, interval]
+  if (strAry.some(n => isType.call(n) !== '[object String]')) {
     _debug(`url/depth/start/end 不是 String 类型`, true)
     return {
       state: false,
@@ -17,7 +17,7 @@ module.exports = ({ url, tags, depth, form, charset, proxyMode, proxies, mode, s
   }
 
   // 判断是否为数组
-  if (Array.isArray(tags) && Array.isArray(proxies)) {
+  if (!Array.isArray(tags) || !Array.isArray(proxies)) {
     _debug(`tags/proxies 不是 Array 类型`, true)
     return {
       state: false,
@@ -35,16 +35,16 @@ module.exports = ({ url, tags, depth, form, charset, proxyMode, proxies, mode, s
   }
 
   // 数据校验
-  const isInvalid = (n) => Number.isNaN(Number.parseInt(n))
-  if ([depth, start, end].some(n => isInvalid(n))) {
-    _debug(`depth/start/end应当可以转化为 Number 类型且不为NaN`, true)
+  const isInvalid = n => Number.isNaN(Number.parseInt(n))
+  if ([depth, start, end, interval].some(n => isInvalid(n))) {
+    _debug(`depth/start/end/应当可以转化为 Number 类型且不为NaN(interval若存在也同理)`, true)
     return {
       state: false,
-      msg: 'depth/start/end应当可以转化为 Number 类型且不为NaN'
+      msg: 'depth/start/end应当可以转化为 Number 类型且不为NaN(interval若存在也同理)'
     }
   }
 
-  if (charset !== 'utf-8' || charset !== 'gbk' || charset !== 'gb2312') {
+  if (['utf-8', 'gbk', 'gb2312'].indexOf(charset) === -1) {
     _debug(`charset应当为 utf-8 或 gbk 或 gb2312`, true)
     return {
       state: false,
@@ -52,7 +52,7 @@ module.exports = ({ url, tags, depth, form, charset, proxyMode, proxies, mode, s
     }
   }
 
-  if (proxyMode !== 'none' || proxyMode !== 'internal' || proxyMode !== 'own') {
+  if (['none', 'internal', 'own'].indexOf(proxyMode) === -1) {
     _debug(`proxyMode 应当为 none 或 internal 或 own`, true)
     return {
       state: false,
