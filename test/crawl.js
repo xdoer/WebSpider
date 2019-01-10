1be37ce4d9002174da5f98fa4f2dae82dd3d9876
LuckyHH <gotoanything@foxmail.com>
2019-01-11 00:06:28 +0800

    新增:爬虫测试用例

---
+++ test/crawl.js
@@ -0,0 +1,20 @@
+/**
+ * 爬虫测试文件
+ */
+const assert = require('assert')
+const crawl = require('../src/crawl')
+
+describe('Crawl', function () {
+  it('爬虫爬取成功', async function () {
+    const res = await crawl({
+      urls: ['https://www.thepaper.cn/'],
+      tags: ["$('.news_li').children('h2').children('a')", "$('.newscontent')"],
+      form: {
+        'title': "$element.children('.news_title').text()",
+        'content': "$element.children('.news_txt').text()"
+      },
+      depth: 2
+    })
+    assert.equal(true, res.state)
+  })
+})
