6d219d6161df7d3dc743890d48525fb109403125
LuckyHH <gotoanything@foxmail.com>
2019-01-19 01:37:08 +0800

    删除url配置项，添加timeout配置项

--- src/config/prod/proxy.js
+++ src/config/prod/proxy.js
@@ -1,12 +1,12 @@
 /**
- * 代理服务器接口
- * url为获取代理服务器的接口.(如果应用部署环境没有写文件权限，该配置项请置空)
+ * 获取代理配置
+ * timeout 为检测可用代理时，测试的延时。单位毫秒。时间越大,可用代理越多，代理质量越不好
  * interval为获取代理服务器的代理的间隔时间(单位:小时)
  * proxy是手动配置的内置代理(填写实例:['http://123.234.345.456','http://123.234.345.456'])
  * 手动配置的代理一般是可靠稳定的，适合抓内网中的数据
  */
 module.exports = {
-  url: 'https://proxys.herokuapp.com/fetchanswer?testTime=1000',
-  interval: 10,
+  timeout: 1000,
+  interval: 1,
   proxy: []
 }
