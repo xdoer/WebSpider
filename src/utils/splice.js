5fa2dea5bd0fa18ef70de12af63ee193694a1cc6
LuckyHH <gotoanything@foxmail.com>
2019-01-19 15:56:23 +0800

    数组拼接

--- src/utils/splice.js
+++ src/utils/splice.js
@@ -3,4 +3,6 @@
  * 拼接多维数组为一维
  */
 
-module.exports = arr => arr.reduce((flat, next) => flat.concat(Array.isArray(next) ? flatten(next) : next), [])
+const flatten = arr => arr.reduce((flat, next) => flat.concat(Array.isArray(next) ? flatten(next) : next), [])
+
+module.exports = flatten
