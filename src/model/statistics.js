495f4f12f054f7e088639a326d9d0f611b77b77b
韩前进 <qianjin.han@hand-china.com>
2019-02-28 16:13:40 +0800

    model 中类属性写法更新

--- src/model/statistics.js
+++ src/model/statistics.js
@@ -59,58 +59,57 @@ class Statistics {
         msg: 'API调用统计初始化失败'
       }))
   }
-}
+  static get (findFlag, option) {
+    return collection.find(findFlag, option)
+      .then(docs => ({
+        state: docs.length > 0,
+        time: new Date().toLocaleString(),
+        data: docs,
+        msg: docs.length > 0 ? '获取成功' : '无数据或获取失败'
+      }))
+      .catch(err => ({
+        state: false,
+        time: new Date().toLocaleString(),
+        data: err,
+        msg: '获取失败'
+      }))
+  }
 
-Statistics.get = (findFlag, option) => {
-  return collection.find(findFlag, option)
-    .then(docs => ({
-      state: docs.length > 0,
-      time: new Date().toLocaleString(),
-      data: docs,
-      msg: docs.length > 0 ? '获取成功' : '无数据或获取失败'
-    }))
-    .catch(err => ({
-      state: false,
-      time: new Date().toLocaleString(),
-      data: err,
-      msg: '获取失败'
-    }))
-}
+  static update (findFlag, newValue) {
+    return collection.update(findFlag, { $set: newValue })
+      .then(docs => ({
+        state: docs.n === 1 && docs.nModified === 1 && docs.ok === 1,
+        time: new Date().toLocaleString(),
+        data: docs,
+        msg: docs.n === 1 && docs.nModified === 1 && docs.ok === 1 ? 'API调用统计信息更新成功' : 'API调用统计信息更新失败'
+      }))
+      .catch((err) => ({
+        state: false,
+        time: new Date().toLocaleString(),
+        data: err,
+        msg: 'API调用统计信息更新失败'
+      }))
+  }
 
-Statistics.update = (findFlag, newValue) => {
-  return collection.update(findFlag, { $set: newValue })
-    .then(docs => ({
-      state: docs.n === 1 && docs.nModified === 1 && docs.ok === 1,
-      time: new Date().toLocaleString(),
-      data: docs,
-      msg: docs.n === 1 && docs.nModified === 1 && docs.ok === 1 ? 'API调用统计信息更新成功' : 'API调用统计信息更新失败'
-    }))
-    .catch((err) => ({
-      state: false,
-      time: new Date().toLocaleString(),
-      data: err,
-      msg: 'API调用统计信息更新失败'
-    }))
-}
-
-Statistics.delete = findFlag => {
-  return collection.remove(findFlag)
-    .then(
-      () => {
-        return collection.find(findFlag).then(docs => ({
-          state: docs.length === 0,
-          time: new Date().toLocaleString(),
-          data: docs.length > 0 ? 'API调用统计信息删除失败' : 'API调用统计信息删除成功',
-          msg: docs.length > 0 ? 'API调用统计信息删除失败' : 'API调用统计信息删除成功'
-        })).catch(err => err)
-      }
-    )
-    .catch(err => ({
-      state: false,
-      time: new Date().toLocaleString(),
-      data: err,
-      msg: 'API调用统计信息删除失败'
-    }))
+  static delete (findFlag) {
+    return collection.remove(findFlag)
+      .then(
+        () => {
+          return collection.find(findFlag).then(docs => ({
+            state: docs.length === 0,
+            time: new Date().toLocaleString(),
+            data: docs.length > 0 ? 'API调用统计信息删除失败' : 'API调用统计信息删除成功',
+            msg: docs.length > 0 ? 'API调用统计信息删除失败' : 'API调用统计信息删除成功'
+          })).catch(err => err)
+        }
+      )
+      .catch(err => ({
+        state: false,
+        time: new Date().toLocaleString(),
+        data: err,
+        msg: 'API调用统计信息删除失败'
+      }))
+  }
 }
 
 module.exports = Statistics
